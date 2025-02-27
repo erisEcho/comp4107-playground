13672da2ec3d6fb0b9c69092c3c4993c21c30e7f
erisEcho <22258361@life.hkbu.edu.hk>
2025-02-27 18:39:36 +0800

    Resolve merge conflicts

--- src/main/kotlin/luyuqi/SelfIntroduction.kt
+++ src/main/kotlin/luyuqi/SelfIntroduction.kt
@@ -2,7 +2,7 @@ package hk.edu.hkbu.comp.comp4107.playground.luyuqi
 
 class SelfIntroduction {
     fun selfIntroduction() {
-        println("Hello, my name is Kenny Cheng.")
+        println("Hello, my name is Lu Yuqi.")
         println("I am a student at HKBU.")
         println("I am studying Computer Science.")
         println("I am interested in machine learning.")
