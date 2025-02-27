a0b458354bbbc31be5725115223f15c484db7b89
erisEcho <22258361@life.hkbu.edu.hk>
2025-02-27 13:46:06 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/luyuqi/SelfIntroduction.kt
@@ -0,0 +1,11 @@
+package hk.edu.hkbu.comp.comp4107.playground.luyuqi
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Kenny Cheng.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
+
